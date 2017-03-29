e4365e95f4d2299d440a59574768e5cdc897dce3
arst <artem.startsev@gmail.com>
2017-03-29 22:18:50 +0300

    Added missing unit tests project

---
+++ Resurrect.Us.Tests/Web/HomeControllerTests.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Resurrect.Us.Data.Models;
+using Resurrect.Us.Data.Services;
+using Resurrect.Us.Web.Controllers;
+using Resurrect.Us.Web.Models;
+using Resurrect.Us.Web.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Resurrect.Us.Tests.Web
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexShouldReturnIndexView()
+        {
+            var waybackMoq = new Mock<IWaybackService>();
+            var keyPointsExtractorMoq = new Mock<IKeyPointsExtractorService>();
+            var storageMoq = new Mock<IResurrectRecordsStorageService>();
+            var sut = new HomeController(waybackMoq.Object,keyPointsExtractorMoq.Object, storageMoq.Object);
+            var viewResult = sut.Index();
+            Assert.NotNull(viewResult as ViewResult);
+            Assert.Equal((viewResult as ViewResult).ViewName, "Index");
+        }
+
+        [Fact]
+        public async Task PostToIndexWithInvalidModelShouldReturnIndexView()
+        {
+            var waybackMoq = new Mock<IWaybackService>();
+            var keyPointsExtractorMoq = new Mock<IKeyPointsExtractorService>();
+            var storageMoq = new Mock<IResurrectRecordsStorageService>();
+            var sut = new HomeController(waybackMoq.Object, keyPointsExtractorMoq.Object, storageMoq.Object);
+            sut.ModelState.AddModelError("Url", "Test error");
+            HomePageViewModel viewModel = new HomePageViewModel();
+            viewModel.Url = "http://test.test";
+            var result = await sut.Index(viewModel);
+            var viewResult = result as ViewResult;
+            Assert.NotNull(viewResult);
+            Assert.Equal(viewResult.ViewName, "Index");
+            Assert.NotNull(viewResult.Model as HomePageViewModel);
+            Assert.Equal((viewResult.Model as HomePageViewModel).Url, "http://test.test");
+        }
+
+        [Fact]
+        public async Task PostToIndexWithValdModelShouldReturnCorrectView()
+        {
+            var waybackMoq = new Mock<IWaybackService>();
+            var keyPointsExtractorMoq = new Mock<IKeyPointsExtractorService>();
+            HTMLKeypointsResult moqKeypoints = new HTMLKeypointsResult()
+            {
+                Title = "Test",
+                Keywords = new List<string>() { "Test1", "Test2", "Test3" }
+            };
+            ResurrectionRecord moqRecord = new ResurrectionRecord()
+            {
+                Id = "Test_id"
+            };
+
+            keyPointsExtractorMoq.Setup(kp => kp.GetHtmlKeypointsFromUrl(It.IsAny<string>())).Returns(Task.FromResult<HTMLKeypointsResult>(moqKeypoints));
+            var storageMoq = new Mock<IResurrectRecordsStorageService>();
+            storageMoq.Setup(s => s.AddRecordAsync(It.IsAny<ResurrectionRecord>())).Returns(Task.FromResult(moqRecord));
+            var sut = new HomeController(waybackMoq.Object, keyPointsExtractorMoq.Object, storageMoq.Object);
+            HomePageViewModel viewModel = new HomePageViewModel();
+            viewModel.Url = "http://test.test";
+            var result = await sut.Index(viewModel);
+            var viewResult = result as ViewResult;
+            Assert.NotNull(viewResult);
+            Assert.Equal(viewResult.ViewName, "Result");
+            Assert.NotNull(viewResult.Model);
+            Assert.Equal(viewResult.Model, "Test_id");
+        }
+
+        [Fact]
+        public async Task PostToIndexWithValdModelShouldCreateNewRecord()
+        {
+            var waybackMoq = new Mock<IWaybackService>();
+            var keyPointsExtractorMoq = new Mock<IKeyPointsExtractorService>();
+            HTMLKeypointsResult moqKeypoints = new HTMLKeypointsResult()
+            {
+                Title = "Test",
+                Keywords = new List<string>() { "Test1", "Test2", "Test3" }
+            };
+            ResurrectionRecord moqRecord = new ResurrectionRecord()
+            {
+                Id = "Test_id"
+            };
+
+            keyPointsExtractorMoq.Setup(kp => kp.GetHtmlKeypointsFromUrl(It.IsAny<string>())).Returns(Task.FromResult<HTMLKeypointsResult>(moqKeypoints));
+            var storageMoq = new Mock<IResurrectRecordsStorageService>();
+            storageMoq.Setup(s => s.AddRecordAsync(It.IsAny<ResurrectionRecord>())).Returns(Task.FromResult(moqRecord));
+            var sut = new HomeController(waybackMoq.Object, keyPointsExtractorMoq.Object, storageMoq.Object);
+            HomePageViewModel viewModel = new HomePageViewModel();
+            viewModel.Url = "http://test.test";
+            var result = await sut.Index(viewModel);
+            storageMoq.Verify(s => s.AddRecordAsync(It.IsAny<ResurrectionRecord>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task PostToIndexWithValdModelShouldCreateNewRecordWithLatestWaybackTimestamp()
+        {
+            var waybackMoq = new Mock<IWaybackService>();
+            var moqWaybackResult = new WaybackResponse()
+            {
+                Closest = new ArchivedSnapshots()
+                {
+                    Closest = new ArchivedSnapshot()
+                    {
+                        Available = true,
+                        Status = 200,
+                        Timestamp = "test_timestamp",
+                        Url = "test_url"
+                    }
+                }
+            };
+            waybackMoq.Setup(w => w.GetWaybackAsync(It.IsAny<string>())).Returns(Task.FromResult(moqWaybackResult));
+            var keyPointsExtractorMoq = new Mock<IKeyPointsExtractorService>();
+            HTMLKeypointsResult moqKeypoints = new HTMLKeypointsResult()
+            {
+                Title = "Test",
+                Keywords = new List<string>() { "Test1", "Test2", "Test3" }
+            };
+            ResurrectionRecord moqRecord = new ResurrectionRecord()
+            {
+                Id = "Test_id"
+            };
+
+            keyPointsExtractorMoq.Setup(kp => kp.GetHtmlKeypointsFromUrl(It.IsAny<string>())).Returns(Task.FromResult<HTMLKeypointsResult>(moqKeypoints));
+            var storageMoq = new Mock<IResurrectRecordsStorageService>();
+            storageMoq.Setup(s => s.AddRecordAsync(It.IsAny<ResurrectionRecord>())).Returns(Task.FromResult(moqRecord));
+            var sut = new HomeController(waybackMoq.Object, keyPointsExtractorMoq.Object, storageMoq.Object);
+            HomePageViewModel viewModel = new HomePageViewModel();
+            viewModel.Url = "http://test.test";
+            var result = await sut.Index(viewModel);
+            storageMoq.Verify(s => s.AddRecordAsync(It.Is<ResurrectionRecord>(r => r.Timestamp == "test_timestamp")), Times.Once);
+        }
+    }
+}
---
+++ Resurrect.Us.Tests/Web/RedirectControllerTests.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Resurrect.Us.Data.Models;
+using Resurrect.Us.Data.Services;
+using Resurrect.Us.Web.Controllers;
+using Resurrect.Us.Web.Models;
+using Resurrect.Us.Web.Service;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Resurrect.Us.Tests.Web
+{
+    public class RedirectControllerTests
+    {
+        [Fact]
+        public void IndexShouldTryToGetRecordWithProvidedId()
+        {
+            var waybackMoq = new Mock<IWaybackService>();
+            var urlCheckerMoq = new Mock<IUrlCheckerService>();
+            var moqWaybackResult = new WaybackResponse()
+            {
+                Closest = new ArchivedSnapshots()
+                {
+                    Closest = new ArchivedSnapshot()
+                    {
+                        Available = true,
+                        Status = 200,
+                        Timestamp = "test_timestamp",
+                        Url = "test_url"
+                    }
+                }
+            };
+            waybackMoq.Setup(w => w.GetWaybackAsync(It.IsAny<string>())).Returns(Task.FromResult(moqWaybackResult));
+            ResurrectionRecord moqRecord = new ResurrectionRecord()
+            {
+                Id = "Test_id"
+            };
+            var storageMoq = new Mock<IResurrectRecordsStorageService>();
+            storageMoq.Setup(s => s.GetResurrectionRecordAsync(It.IsAny<string>())).Returns(() => null);
+            var sut = new RedirectController(storageMoq.Object, waybackMoq.Object, urlCheckerMoq.Object);
+            var result = sut.Index("test_id");
+            storageMoq.Verify(s => s.GetResurrectionRecordAsync(It.Is<string>(id => id == "test_id")), Times.Once);
+        }
+
+
+        [Fact]
+        public async Task IndexShouldRedirectToHomeIndexWhenRecordIsNotFound()
+        {
+            var waybackMoq = new Mock<IWaybackService>();
+            var urlCheckerMoq = new Mock<IUrlCheckerService>();
+            var moqWaybackResult = new WaybackResponse()
+            {
+                Closest = new ArchivedSnapshots()
+                {
+                    Closest = new ArchivedSnapshot()
+                    {
+                        Available = true,
+                        Status = 200,
+                        Timestamp = "test_timestamp",
+                        Url = "test_url"
+                    }
+                }
+            };
+            waybackMoq.Setup(w => w.GetWaybackAsync(It.IsAny<string>())).Returns(Task.FromResult(moqWaybackResult));
+            ResurrectionRecord moqRecord = new ResurrectionRecord()
+            {
+                Id = "Test_id"
+            };
+            var storageMoq = new Mock<IResurrectRecordsStorageService>();
+            storageMoq.Setup(s => s.GetResurrectionRecordAsync(It.IsAny<string>())).Returns(() => null);
+            var sut = new RedirectController(storageMoq.Object, waybackMoq.Object, urlCheckerMoq.Object);
+            var result = (await sut.Index("test_id")) as RedirectToActionResult;
+
+            Assert.Equal(result.ActionName, "Index");
+            Assert.Equal(result.ControllerName, "Home");
+            Assert.Equal(result.Permanent, false);
+        }
+
+        [Fact]
+        public async Task IndexShouldCheckCorrectUrlWhenRecordIsFound()
+        {
+            var waybackMoq = new Mock<IWaybackService>();
+            var urlCheckerMoq = new Mock<IUrlCheckerService>();
+            var moqWaybackResult = new WaybackResponse()
+            {
+                Closest = new ArchivedSnapshots()
+                {
+                    Closest = new ArchivedSnapshot()
+                    {
+                        Available = true,
+                        Status = 200,
+                        Timestamp = "test_timestamp",
+                        Url = "test_url"
+                    }
+                }
+            };
+            waybackMoq.Setup(w => w.GetWaybackAsync(It.IsAny<string>())).Returns(Task.FromResult(moqWaybackResult));
+            ResurrectionRecord moqRecord = new ResurrectionRecord()
+            {
+                Id = "Test_id",
+                Url = "test_url"
+            };
+            var storageMoq = new Mock<IResurrectRecordsStorageService>();
+            storageMoq.Setup(s => s.GetResurrectionRecordAsync(It.IsAny<string>())).Returns(() => moqRecord);
+            var sut = new RedirectController(storageMoq.Object, waybackMoq.Object, urlCheckerMoq.Object);
+            var result = (await sut.Index("test_id")) as RedirectToActionResult;
+            urlCheckerMoq.Verify(u => u.CheckUrl(It.Is<string>(url => url == "test_url")));
+        }
+
+        [Fact]
+        public async Task IndexShouldRedirectToRecordUrlWhenUrlIsAvailable()
+        {
+            var waybackMoq = new Mock<IWaybackService>();
+            var urlCheckerMoq = new Mock<IUrlCheckerService>();
+            urlCheckerMoq.Setup(c => c.CheckUrl(It.IsAny<string>())).Returns(Task.FromResult<HttpStatusCode>(HttpStatusCode.OK));
+            var moqWaybackResult = new WaybackResponse()
+            {
+                Closest = new ArchivedSnapshots()
+                {
+                    Closest = new ArchivedSnapshot()
+                    {
+                        Available = true,
+                        Status = 200,
+                        Timestamp = "test_timestamp",
+                        Url = "test_url"
+                    }
+                }
+            };
+            waybackMoq.Setup(w => w.GetWaybackAsync(It.IsAny<string>())).Returns(Task.FromResult(moqWaybackResult));
+            ResurrectionRecord moqRecord = new ResurrectionRecord()
+            {
+                Id = "Test_id",
+                Url = "test_url"
+            };
+            var storageMoq = new Mock<IResurrectRecordsStorageService>();
+            storageMoq.Setup(s => s.GetResurrectionRecordAsync(It.IsAny<string>())).Returns(() => moqRecord);
+            var sut = new RedirectController(storageMoq.Object, waybackMoq.Object, urlCheckerMoq.Object);
+            var result = (await sut.Index("test_id")) as RedirectResult;
+            Assert.Equal(result.Url, "test_url");
+            Assert.Equal(result.Permanent, true);
+        }
+
+        [Fact]
+        public async Task IndexShouldRedirectToWaybackUrlWhenUrlIsNotAvailable()
+        {
+            var waybackMoq = new Mock<IWaybackService>();
+            var urlCheckerMoq = new Mock<IUrlCheckerService>();
+            urlCheckerMoq.Setup(c => c.CheckUrl(It.IsAny<string>())).Returns(Task.FromResult<HttpStatusCode>(HttpStatusCode.NotFound));
+            var moqWaybackResult = new WaybackResponse()
+            {
+                Closest = new ArchivedSnapshots()
+                {
+                    Closest = new ArchivedSnapshot()
+                    {
+                        Available = true,
+                        Status = 200,
+                        Timestamp = "test_timestamp",
+                        Url = "wayback_test_url"
+                    }
+                }
+            };
+            waybackMoq.Setup(w => w.GetWaybackAsync(It.IsAny<string>())).Returns(Task.FromResult(moqWaybackResult));
+            ResurrectionRecord moqRecord = new ResurrectionRecord()
+            {
+                Id = "Test_id",
+                Url = "test_url"
+            };
+            var storageMoq = new Mock<IResurrectRecordsStorageService>();
+            storageMoq.Setup(s => s.GetResurrectionRecordAsync(It.IsAny<string>())).Returns(() => moqRecord);
+            var sut = new RedirectController(storageMoq.Object, waybackMoq.Object, urlCheckerMoq.Object);
+            var result = (await sut.Index("test_id")) as RedirectResult;
+            Assert.Equal(result.Url, "wayback_test_url");
+            Assert.Equal(result.Permanent, false);
+        }
+
+
+
+
+    }
+}
