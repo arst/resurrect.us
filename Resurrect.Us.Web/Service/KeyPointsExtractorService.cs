92193f1787a9748cdca430a1f769ffb6367e415b
arst <artem.startsev@gmail.com>
2017-04-05 22:47:12 +0300

    Check if url is available before extracting a keypoints

--- Resurrect.Us.Web/Service/KeyPointsExtractorService.cs
+++ Resurrect.Us.Web/Service/KeyPointsExtractorService.cs
@@ -10,19 +10,26 @@ namespace Resurrect.Us.Web.Service
     public class KeyPointsExtractorService : IKeyPointsExtractorService
     {
         private readonly IDOMProcessingService domProcessingService;
+        private readonly IUrlCheckerService urlChecker;
 
-        public KeyPointsExtractorService(IDOMProcessingService domProcessingService)
+        public KeyPointsExtractorService(IDOMProcessingService domProcessingService, IUrlCheckerService urlChecker)
         {
             this.domProcessingService = domProcessingService;
+            this.urlChecker = urlChecker;
         }
 
         public async Task<HTMLKeypointsResult> GetHtmlKeypointsFromUrl(string url)
         {
             HttpClient cl = new HttpClient();
-            var html = await cl.GetStringAsync(url);
-            var keypoints = this.domProcessingService.ExtractHTMLKeypoints(html);
 
-            return keypoints;
+            if (await urlChecker.CheckUrl(url) == System.Net.HttpStatusCode.OK)
+            {
+                var html = await cl.GetStringAsync(url);
+                var keypoints = this.domProcessingService.ExtractHTMLKeypoints(html);
+
+                return keypoints;
+            }
+            return new HTMLKeypointsResult();
         }
     }
 }
